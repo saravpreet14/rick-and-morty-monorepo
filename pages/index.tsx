809437f83031baf4df09cf7608a6ad694ef08754
Saravpreet Singh <saravpreet14@gmail.com>
2021-07-07 10:45:34 +0530

    static state management on routing

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,8 @@ import Navbar from "../components/navbar/navbar";
 import Episodes from '../components/episodes/episodes';
 import Widgets from '../components/widgets/widgets';
 
+let staticEpisodeData = null;
+
 export default function SignInPage() {
   const [session, loading] = useSession();
 
@@ -12,16 +14,16 @@ export default function SignInPage() {
   if (session) authFunction = signOut;
   else authFunction = signIn;
 
-  const [episodeData, setEpisode] = useState(null);
+  const [episodeData, setEpisode] = useState(staticEpisodeData);
 
-  console.log(episodeData);
+  // console.log(episodeData);
 
   return (
     <>
       <Navbar auth={() => authFunction()} isAuth={session ? true : false}>      
         <div style={{display: 'grid', gridTemplateColumns: '2fr 8fr', marginLeft: '1vw', marginRight: '3vw'}} >
-          <Episodes select={(data) => setEpisode(data)} selected={episodeData ? episodeData.id : null} />
-          {episodeData ? <Widgets data={episodeData} back={() => setEpisode(null)} /> : <Home imageSize={{width: 300, height: 300}} />}
+          <Episodes select={(data) => {staticEpisodeData=data; setEpisode(data);}} selected={episodeData ? episodeData.id : null} />
+          {episodeData ? <Widgets data={episodeData} back={() => {staticEpisodeData=null; setEpisode(null);}} /> : <Home imageSize={{width: 300, height: 300}} />}
         </div>
       </Navbar>
     </>
