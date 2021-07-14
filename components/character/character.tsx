433d36add55fe5e3331a3fd3a28889f611ddf2ee
Phanindra180 <phanindrasai180@gmail.com>
2021-07-14 23:05:37 +0530

    added back button and linked episodes in characters page

--- components/character/character.tsx
+++ components/character/character.tsx
@@ -3,7 +3,8 @@ import Image from "next/image";
 import Link from "next/link";
 import styles from "./MyCharacter.module.css";
 import { useQuery, gql } from "@apollo/client";
-import { Button, CircularProgress } from "@material-ui/core";
+import { IconButton,Button, CircularProgress } from "@material-ui/core";
+import { ArrowBackIosRounded } from "@material-ui/icons";
 import Error from "../error/error";
 import { classicNameResolver } from "typescript";
 import Episodes from "../episodes/episodes";
@@ -35,6 +36,8 @@ interface dataFromApi {
   loading?: boolean;
   error?: any;
 }
+
+const colormap = {"Alive":"green","Dead":"red","unknown":"gray"};
 
 export default function MyCharacter(props: { params: { id: string } }) {
   const params = props.params;
@@ -75,50 +78,59 @@ export default function MyCharacter(props: { params: { id: string } }) {
   const { name, image, gender, location, origin, species, status,episode } =
     data.charactersByIds[0];
   return (
-    <div className={styles.root} >
-      <div className={styles.container1}>
-        <div className={styles.profile_image}>
-          <Image src={image} alt={name} width="300" height="300" />
+    <div className={styles.top}>
+      <div className={styles.iconDiv}>
+        <IconButton onClick={() => window.history.back() } className={styles.iconBack} style={{backgroundColor: 'white', borderRadius: '2px', padding: '2px'}} aria-label="menu">
+          <ArrowBackIosRounded />Back
+        </IconButton>
+      </div>
+      <div className={styles.root} >
+        <div className={styles.container1}>
+          <div className={styles.profile_image}>
+            <Image src={image} alt={name} width="300" height="300" />
+          </div>
+          <h2 className={styles.name}>{name}</h2>
+          <div className={styles.info}>
+            <div className={styles.heading_box}>
+              <h2 className={styles.heading}>INFO</h2>
+              <div className={styles.line}></div>
+            </div>
+            <div {...{style:{display:"flex"}}} >
+              <div {...{style:{borderRadius:"50%",border:"solid 5px",width:"10px",height:"10px",borderColor:colormap[status]}}}></div>
+              <div>{`${status} - ${species}`}</div>
+            </div>
+            <div className={styles.section}>
+              <div className={styles.sub_heading}>
+                Last known location:
+              </div>
+              <div >
+                {location.name}
+              </div>
+            </div>
+            <div className={styles.section}>
+              <div className={styles.sub_heading}>
+                First seen in:
+              </div>
+              <div>
+                {origin.name}
+              </div>
+            </div>
+          </div>
         </div>
-        <h2 className={styles.name}>{name}</h2>
-        <div className={styles.info}>
+        <div className={styles.container2}>
           <div className={styles.heading_box}>
-            <h2 className={styles.heading}>INFO</h2>
+            <h2 className={styles.heading}>FEATURED IN</h2>
             <div className={styles.line}></div>
           </div>
-          <div>
-            {/* <div className={styles({status})}></div> */}
-           <div>{`${status} - ${species}`}</div>
-          </div>
-          <div className={styles.section}>
-            <div className={styles.sub_heading}>
-              Last known location:
-            </div>
-            <div >
-              {location.name}
-            </div>
-          </div>
-          <div className={styles.section}>
-            <div className={styles.sub_heading}>
-              First seen in:
-            </div>
-            <div>
-              {origin.name}
-            </div>
-          </div>
+          {episode.map((episode) => (
+            <Link href={"/episode/" + episode.episode + "-" +episode.id} key={episode.id}  passHref>
+              <div className={styles.episode}>            
+                <p className={styles.episode_name}>{`${episode.episode} - ${episode.name}`}</p>
+                <p className={styles.date}>{episode.air_date}</p>
+              </div>
+            </Link>
+          ))}
         </div>
-      </div>
-      <div className={styles.container2}>
-        <div className={styles.heading_box}>
-          <h2 className={styles.heading}>FEATURED IN</h2>
-          <div className={styles.line}></div>
-        </div>
-        {episode.map((episode) => (
-          <div key={episode.id} className={styles.episode}>
-            <p className={styles.episode_name}>{`${episode.episode} - ${episode.name}`}</p>
-            <p className={styles.date}>{episode.air_date}</p>
-          </div>
-        ))}
       </div>
     </div>
   );
