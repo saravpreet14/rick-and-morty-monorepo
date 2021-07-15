5ad1d1e3998b18289d0daf3ea23abce6a1eab6c6
Phanindra180 <phanindrasai180@gmail.com>
2021-07-15 18:00:54 +0530

    changed characters page UI

--- components/character/character.tsx
+++ components/character/character.tsx
@@ -8,6 +8,8 @@ import { ArrowBackIosRounded } from "@material-ui/icons";
 import Error from "../error/error";
 import { classicNameResolver } from "typescript";
 import Episodes from "../episodes/episodes";
+import Skeleton from 'react-loading-skeleton';
+
 
 interface dataFromApi {
   data: {
@@ -73,52 +75,34 @@ export default function MyCharacter(props: { params: { id: string } }) {
   });
   const { loading, error, data } = responseData;
   if (loading) {
-    return <div className={styles.spinner} ><CircularProgress /></div>;
-  }
+    return (      
+      <div className={styles.root} >
+        <div className={styles.container1} style={{borderColor: 'white'}}>
+        <Skeleton count={1} height={570} width={350} />
+        </div>
+        <div className={styles.container2}>
+        <Skeleton count={1} height={900} width={330} />
+        </div>
+      </div>
+    )
+  ;}
   if (error) return <Error />;
   // console.log(data.charactersByIds[0]);
+  
   const { name, image, gender, location, origin, species, status,episode } =
     data.charactersByIds[0];
   return (
-    <div className={styles.top}>
-      <div className={styles.iconDiv}>
-        <IconButton onClick={() => window.history.back() } className={styles.iconBack} style={{backgroundColor: 'white', borderRadius: '2px', padding: '2px'}} aria-label="menu">
-          <ArrowBackIosRounded />Back
-        </IconButton>
-      </div>
+
       <div className={styles.root} >
         <div className={styles.container1}>
           <div className={styles.profile_image}>
             <div className={styles.image}>
               <Image src={image} alt={name} width="300" height="300" />
             </div>
-    {/* <div className={styles.mainBlock} >
-      <IconButton onClick={() => window.history.back() } className={styles.iconBack} style={{backgroundColor: 'white', borderRadius: '2px', padding: '2px'}} aria-label="menu">
-        <ArrowBackIosRounded />Back
-      </IconButton>
-    <div className={styles.root} >
-      <div className={styles.container1}>
-        <div className={styles.profile_image}>
-          <Image src={image} alt={name} width="300" height="300" />
-        </div>
-        <h2 className={styles.name}>{name}</h2>
-        <div className={styles.info}>
-          <div className={styles.heading_box}>
-            <h2 className={styles.heading}>INFO</h2>
-            <div className={styles.line}></div>
-          </div>
-          <div>
-            {/* <div className={styles({status})}></div> /}
-           <div>{`${status} - ${species}`}</div>
-          </div> */}
           <h2 className={styles.name}>{name}</h2>
           <div className={styles.info}>
-            {/* <div className={styles.heading_box}>
-              <h2 className={styles.heading}>INFO</h2>
-              <div className={styles.line}></div>
-            </div> */}
             <div {...{style:{display:"flex"}}} >
-              <div {...{style:{borderRadius:"50%",border:"solid 5px",width:"10px",height:"10px",borderColor:colormap[status], margin: 'auto 0'}}}></div>
+              <div {...{style:{borderRadius:"50%",border:"solid 5px",width:"10px",height:"10px",borderColor:colormap[status], margin: 'auto 2px'}}}></div>
               <div>{`${status} - ${species}`}</div>
             </div>
             <div className={styles.section}>
@@ -143,7 +127,7 @@ export default function MyCharacter(props: { params: { id: string } }) {
       <div className={styles.container2}>
         <div className={styles.heading_box}>
           <h2 className={styles.heading}>FEATURED IN</h2>
-          <div className={styles.line}></div>
+          {/* <div className={styles.line}></div> */}
         </div>
         {episode.map((episode) => (
           <Link href={`/episode/${episode.id}`} passHref key={episode.id} >
@@ -154,7 +138,6 @@ export default function MyCharacter(props: { params: { id: string } }) {
           </Link>
         ))}
       </div>
-    </div>
     </div>
   );
 }
