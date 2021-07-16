48fa3c14ad08c3ead17c40a38b4d35fc6ff5dee6
Saravpreet Singh <saravpreet14@gmail.com>
2021-07-16 13:10:05 +0530

    added partition in episode page

--- pages/episode/[id].tsx
+++ pages/episode/[id].tsx
@@ -1,38 +1,3 @@
-// import Character from "../../components/character/character";
-// import { signIn, signOut, useSession } from "next-auth/client";
-// import Navbar from "../../components/navbar/navbar";
-// import { GetServerSideProps } from "next";
-// import Widgets from '../../components/widgets/widgets';
-// import Episodes from '../../components/episodes/episodes';
-// import styles from '../../styles/Home.module.css';
-
-// export const getServerSideProps: GetServerSideProps = async ({ params }) => {
-//   return {
-//     props: {
-//       params,
-//     },
-//   };
-// };
-
-// export default function MyEpisode({
-//   params,
-// }: {
-//   params: {
-//     id: string;
-//   };
-// }) {
-//   console.log("MyEpisode",params);
-  
-//   return (
-//     <Navbar >
-//       <div className={styles.partition} >
-//         <Episodes selected={params.id} placeholder="Search Episode Name"/>
-//         <Widgets {...{ params }} />
-//       </div>
-//     </Navbar>
-//   );
-// }
-
 import Widgets from '../../components/widgets/widgets';
 import Navbar from "../../components/navbar/navbar";
 import { useRouter } from 'next/router';
@@ -47,8 +12,10 @@ export default function MyCharacter(props) {
 
   return (
     <Navbar>
-      <Episodes selected={id} placeholder="Search Episode Name"/>
-      <Widgets params={{id: id}} />
+      <div className={styles.partition} >
+        <Episodes selected={id} placeholder="Search Episode Name"/>
+        <Widgets params={{id: id}} />
+      </div>
     </Navbar>
   );
 }
