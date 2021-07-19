45f22497ad53bd9e8efa76f8f16264a0c4b1393e
Saravpreet Singh <saravpreet14@gmail.com>
2021-07-19 10:35:53 +0530

    measuring web vitals

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,20 +8,28 @@ import { AppProps,NextWebVitalsMetric } from "next/app";
 // import { onError } from "@apollo/client/link/error";
 // import { Provider } from "next-auth/client";
 // import axios from 'axios';
-// import printReadings from '../readings.js'
+import printReadings from '../lib/readings.js'
 const client = new ApolloClient({
   uri: "https://rickandmortyapi.com/graphql/",
   cache: new InMemoryCache(),
   // link: errorLink,
 });
 
-const readingsDatabse = "https://rick-and-morty-22d4d-default-rtdb.firebaseio.com/NextApp";
+const readingsDatabse = "https://ricky-and-morty-project-default-rtdb.asia-southeast1.firebasedatabase.app/NextApp";
 
  export function reportWebVitals(metric: NextWebVitalsMetric) {
   if (['FCP', 'LCP', 'CLS', 'FID', 'TTFB'].includes(metric.name)) {
-    // console.log(metric.name, metric.value);
-    // axios.post(`${readingsDatabse}/${metric.name}.json`, metric.value);
-    // printReadings(readingsDatabse + '.json');
+    console.log(metric.name, metric.value);
+    // axios.post(`${readingsDatabse}/${metric.name}.json`, metric.value).catch(() => null);
+    fetch(`${readingsDatabse}/${metric.name}.json`, {
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      method: "POST",
+      body: `${metric.value.toFixed(6)}`
+    })
+    printReadings(readingsDatabse + '.json');
   }
  }
   
