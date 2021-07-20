593d68b3d823d25189c2351325b4eb18d7413ca2
Saravpreet Singh <saravpreet14@gmail.com>
2021-07-20 11:55:30 +0530

    updated error message

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -21,15 +21,15 @@ const readingsDatabse = "https://ricky-and-morty-project-default-rtdb.asia-south
   if (['FCP', 'LCP', 'CLS', 'FID', 'TTFB'].includes(metric.name)) {
     console.log(metric.name, metric.value);
     // axios.post(`${readingsDatabse}/${metric.name}.json`, metric.value).catch(() => null);
-    fetch(`${readingsDatabse}/${metric.name}.json`, {
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      method: "POST",
-      body: `${metric.value.toFixed(6)}`
-    })
-    printReadings(readingsDatabse + '.json');
+    // fetch(`${readingsDatabse}/${metric.name}.json`, {
+    //   headers: {
+    //     'Accept': 'application/json',
+    //     'Content-Type': 'application/json'
+    //   },
+    //   method: "POST",
+    //   body: `${metric.value.toFixed(6)}`
+    // }).catch(error => null)
+    // printReadings(readingsDatabse + '.json');
   }
  }
   
